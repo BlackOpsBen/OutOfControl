23f468f023472b00b1acd8e219ddacb53cdf100e
Ben Mora <blackopsben@gmail.com>
2020-07-10 20:49:59 -0500

    Created Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        SingletonPattern();
+        CreateAudioSources();
+    }
+
+    private void CreateAudioSources()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    private void SingletonPattern()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
